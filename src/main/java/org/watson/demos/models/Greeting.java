f9306963ff12dcbdd10244b235ca5a4b39c886b0
Watson <ryanwatson@salesforce.com>
2022-10-05 18:07:34 -0700

    Add created field to Greeting. Add test coverage. Fix UUID delete failure bug.

--- src/main/java/org/watson/demos/models/Greeting.java
+++ src/main/java/org/watson/demos/models/Greeting.java
@@ -1,12 +1,27 @@
 package org.watson.demos.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
 import org.watson.demos.validation.constraints.ValidLocale;
 
-import javax.persistence.*;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
+import java.time.ZonedDateTime;
 import java.util.Locale;
 import java.util.UUID;
 
@@ -21,6 +36,7 @@ public class Greeting implements Serializable {
     @Id
     @GeneratedValue
     @Schema(accessMode = Schema.AccessMode.READ_ONLY)
+    @Column(columnDefinition = "uuid", updatable = false, length = 36)
     private UUID id;
 
     @NotBlank
@@ -31,4 +47,15 @@ public class Greeting implements Serializable {
     @Builder.Default
     @Schema(type = "string", format = "locale")
     private Locale locale = Locale.getDefault();
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "UTC", shape = JsonFormat.Shape.STRING)
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
+    @Basic(optional = false)
+    @CreatedDate
+    private ZonedDateTime created;
+
+    @PrePersist
+    protected void onCreate() {
+        created = ZonedDateTime.now().withNano(0);
+    }
 }
--- src/test/java/org/watson/demos/repositories/GreetingRepositoryTest.java
+++ src/test/java/org/watson/demos/repositories/GreetingRepositoryTest.java
@@ -3,14 +3,20 @@ package org.watson.demos.repositories;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.watson.demos.models.Greeting;
 
 import javax.annotation.Resource;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.StreamSupport;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -27,13 +33,73 @@ class GreetingRepositoryTest {
     private GreetingRepository repository;
 
     @Test
-    void testStuff() {
+    void saveAll_writesToRepository() {
         repository.saveAll(TEST_VALUES);
 
         final Page<Greeting> actual = repository.findAll(Pageable.unpaged());
 
         assertThat(actual).isNotNull();
         assertThat(actual.getTotalElements()).isEqualTo(TEST_VALUES.size());
-        assertThat(actual.get().collect(Collectors.toList())).isEqualTo(TEST_VALUES);
+        assertThat(actual.get().collect(Collectors.toList())).containsExactlyInAnyOrderElementsOf(TEST_VALUES);
+    }
+
+    @Test
+    void saveGreeting_setsIdAndCreatedDate() {
+        final Greeting savedEntry = repository.save(Greeting.builder().content("some entry").locale(Locale.getDefault()).build());
+        assertThat(savedEntry.getId()).isNotNull();
+        assertThat(savedEntry.getCreated()).isBetween(ZonedDateTime.now(ZoneId.of("UTC")).minusSeconds(5), ZonedDateTime.now(ZoneId.of("UTC")));
+
+        final Optional<Greeting> actual = repository.findById(savedEntry.getId());
+        assertThat(actual).isPresent();
+        assertThat(actual.get().getCreated()).isBetween(ZonedDateTime.now(ZoneId.of("UTC")).minusSeconds(5), ZonedDateTime.now(ZoneId.of("UTC")));
+    }
+
+    @Test
+    void findAllByLocal_returnsEntries() {
+        final List<Greeting> expected = saveAll(
+                Greeting.builder().content("oh").locale(Locale.getDefault()).build(),
+                Greeting.builder().content("hello").locale(Locale.getDefault()).build(),
+                Greeting.builder().content("there").locale(Locale.getDefault()).build());
+
+        repository.save(Greeting.builder().content("stuff").locale(Locale.CANADA_FRENCH).build());
+
+        final Page<Greeting> actual = repository.findAllByLocale(Locale.getDefault(), Pageable.unpaged());
+
+        assertThat(actual.getTotalElements()).isEqualTo(expected.size());
+        assertThat(actual.getContent()).isEqualTo(expected);
+    }
+
+    @Test
+    void findAllByLocale_pagesAsExpected() {
+        final List<Greeting> expected = saveAll(
+                Greeting.builder().content("hello").locale(Locale.getDefault()).build(),
+                Greeting.builder().content("there").locale(Locale.getDefault()).build(),
+                Greeting.builder().content("friend").locale(Locale.getDefault()).build());
+
+        for (int i = 0; i < expected.size(); i++) {
+            final Page<Greeting> actual = repository.findAllByLocale(Locale.getDefault(), PageRequest.of(i, 1));
+
+            assertThat(actual.getNumberOfElements()).isEqualTo(1);
+            assertThat(actual.getTotalElements()).isEqualTo(expected.size());
+            assertThat(actual.getContent()).isEqualTo(List.of(expected.get(i)));
+        }
+    }
+
+    @Test
+    void deleteById_deletesEntries() {
+        final Greeting savedEntry = Greeting.builder().content("first entry").locale(Locale.getDefault()).build();
+        final Greeting secondEntry = Greeting.builder().content("second entry").locale(Locale.getDefault()).build();
+        saveAll(savedEntry, secondEntry);
+        repository.deleteById(savedEntry.getId());
+
+        final Page<Greeting> actual = repository.findAllByLocale(Locale.getDefault(), Pageable.unpaged());
+        assertThat(actual).anyMatch(n -> n.getId().equals(secondEntry.getId()));
+        assertThat(actual).noneMatch(n -> n.getId().equals(savedEntry.getId()));
+    }
+
+    private List<Greeting> saveAll(final Greeting... entries) {
+        return StreamSupport
+                .stream(repository.saveAll(Arrays.asList(entries)).spliterator(), false)
+                .collect(Collectors.toList());
     }
 }
