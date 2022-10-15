021dec98fce4ab5b6f3f71d62e195d96a79b3073
Watson <ryanwatson@salesforce.com>
2022-10-14 23:42:12 -0700

    Set delete to NO_CONTENT

--- src/main/java/org/watson/demos/controllers/GreetingController.java
+++ src/main/java/org/watson/demos/controllers/GreetingController.java
@@ -8,7 +8,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.watson.demos.models.Greeting;
 import org.watson.demos.models.GreetingProbe;
@@ -44,6 +52,7 @@ public class GreetingController {
         return service.createAll(greetings);
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping
     public void deleteGreetings(@RequestParam final Set<UUID> id) {
         service.deleteAll(id);
