95ea882cae7f5b1c7888e37f17f7c54cb5bbe5be
Watson <ryanwatson@salesforce.com>
2022-12-20 18:20:42 -0800

    Set the server zone to UTC to workaround Locale/TimeZone issues with spring-graphql

--- src/main/java/org/watson/demos/Application.java
+++ src/main/java/org/watson/demos/Application.java
@@ -5,12 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.time.ZoneOffset;
+import java.util.TimeZone;
+
 @EnableAsync(proxyTargetClass = true)
 @EnableScheduling
 @SpringBootApplication
 public class Application {
 
     public static void main(final String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone(ZoneOffset.UTC));
         SpringApplication.run(Application.class, args);
     }
 }
