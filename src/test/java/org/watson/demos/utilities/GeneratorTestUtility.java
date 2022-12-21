a6e20f0a4c71f0172d6bacb3c9a29308600d29c1
Watson <ryanwatson@salesforce.com>
2022-12-20 18:26:57 -0800

    Filter-out wacky locales

--- src/test/java/org/watson/demos/utilities/GeneratorTestUtility.java
+++ src/test/java/org/watson/demos/utilities/GeneratorTestUtility.java
@@ -5,6 +5,7 @@ import lombok.experimental.UtilityClass;
 import org.watson.demos.models.Greeting;
 import org.watson.demos.models.Identifiable;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
@@ -13,13 +14,15 @@ import java.util.stream.IntStream;
 @UtilityClass
 public class GeneratorTestUtility {
 
-    private static final Locale[] AVAILABLE_LOCALES = Locale.getAvailableLocales();
+    private static final List<Locale> AVAILABLE_LOCALES = Arrays.stream(Locale.getAvailableLocales())
+            .filter(l -> !(l.toString().contains("#") || l.toString().isEmpty()))
+            .collect(Collectors.toList());
     private static final int CONTENT_COUNT = 10;
 
     public static List<Greeting> generateGreetings(final String content) {
         return IntStream.range(0, CONTENT_COUNT).boxed()
                 .map(i -> Greeting.builder()
-                        .locale(AVAILABLE_LOCALES[i % AVAILABLE_LOCALES.length])
+                        .locale(AVAILABLE_LOCALES.get(i % AVAILABLE_LOCALES.size()))
                         .content(content + " " + i))
                 .map(Greeting.GreetingBuilder::build)
                 .collect(Collectors.toUnmodifiableList());
