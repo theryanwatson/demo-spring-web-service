aeb63f0b9036617186d105c8a172e56184ed1516
Watson <ryanwatson@salesforce.com>
2022-12-22 10:13:38 -0800

    Add Actuator InMem Event Repos

---
+++ src/main/java/org/watson/demos/configuration/EventRepositoryConfiguration.java
@@ -0,0 +1,35 @@
+package org.watson.demos.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.audit.AuditEventRepository;
+import org.springframework.boot.actuate.audit.InMemoryAuditEventRepository;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Optional;
+
+@Configuration(proxyBeanMethods = false)
+public class EventRepositoryConfiguration {
+    @ConditionalOnMissingBean
+    @ConditionalOnProperty(prefix = "management.auditevents", name = {"enabled", "repository.enabled"}, matchIfMissing = true)
+    @Bean
+    public AuditEventRepository auditEventRepository(@Value("${management.auditevents.repository.event.capacity:#{null}}") final Optional<Integer> capacity) {
+        return capacity.map(InMemoryAuditEventRepository::new)
+                .orElseGet(InMemoryAuditEventRepository::new);
+    }
+
+    @ConditionalOnMissingBean
+    @ConditionalOnProperty(prefix = "management.httpexchanges.recording", name = {"enabled", "repository.enabled"}, matchIfMissing = true)
+    @ConditionalOnWebApplication
+    @Bean
+    public HttpTraceRepository httpExchangeRepository(@Value("${management.httpexchanges.recording.repository.event.capacity:#{null}}") final Optional<Integer> capacity) {
+        return new InMemoryHttpTraceRepository() {{
+            capacity.ifPresent(this::setCapacity);
+        }};
+    }
+}
---
+++ src/test/java/org/watson/demos/configuration/EventRepositoryConfigurationTest.java
@@ -0,0 +1,60 @@
+package org.watson.demos.configuration;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.boot.actuate.audit.AuditEventRepository;
+import org.springframework.boot.actuate.audit.InMemoryAuditEventRepository;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.boot.test.context.runner.WebApplicationContextRunner;
+
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EventRepositoryConfigurationTest {
+
+    private final WebApplicationContextRunner contextRunner = new WebApplicationContextRunner()
+            .withUserConfiguration(EventRepositoryConfiguration.class);
+
+    @ValueSource(strings = {"auditEventRepository", "httpExchangeRepository"})
+    @ParameterizedTest
+    void enabledByDefault(final String beanName) {
+        contextRunner.withPropertyValues("spring.config.location=classpath:empty.properties")
+                .run(context -> assertThat(context).hasBean(beanName));
+    }
+
+    @ValueSource(strings = {"management.auditevents.enabled=false", "management.auditevents.repository.enabled=false"})
+    @ParameterizedTest
+    void auditEventRepository_disabledByProperty(final String property) {
+        contextRunner.withPropertyValues("spring.config.location=classpath:empty.properties")
+                .withPropertyValues(property)
+                .run(context -> assertThat(context).doesNotHaveBean("auditEventRepository"));
+    }
+
+    @ValueSource(strings = {"management.httpexchanges.recording.enabled=false", "management.httpexchanges.recording.repository.enabled=false"})
+    @ParameterizedTest
+    void httpExchangeRepository_disabledByProperty(final String property) {
+        contextRunner.withPropertyValues("spring.config.location=classpath:empty.properties")
+                .withPropertyValues(property)
+                .run(context -> assertThat(context).doesNotHaveBean("httpExchangeRepository"));
+    }
+
+    @MethodSource
+    @ParameterizedTest
+    <T> void auditEventRepository_disabledByExistingBean(Class<T> type, Supplier<T> supplier) {
+        contextRunner.withPropertyValues("spring.config.location=classpath:empty.properties")
+                .withBean(type, supplier)
+                .run(context -> assertThat(context).hasSingleBean(type));
+    }
+
+    static Stream<Arguments> auditEventRepository_disabledByExistingBean() {
+        return Stream.of(
+                Arguments.of(AuditEventRepository.class, (Supplier<?>) InMemoryAuditEventRepository::new),
+                Arguments.of(HttpTraceRepository.class, (Supplier<?>) InMemoryHttpTraceRepository::new)
+        );
+    }
+}
