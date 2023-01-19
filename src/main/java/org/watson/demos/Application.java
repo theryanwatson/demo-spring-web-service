ff048b98d141f31761d25f9ab077ce1da3da9431
Watson <ryanwatson@salesforce.com>
2023-01-19 14:17:33 -0800

    Clean up Application main config annotations. Best practice in separate configuration files. Make configs able to be disabled.

--- src/main/java/org/watson/demos/Application.java
+++ src/main/java/org/watson/demos/Application.java
@@ -2,14 +2,10 @@ package org.watson.demos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.time.ZoneOffset;
 import java.util.TimeZone;
 
-@EnableAsync(proxyTargetClass = true)
-@EnableScheduling
 @SpringBootApplication
 public class Application {
 
