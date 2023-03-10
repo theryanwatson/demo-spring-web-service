5d7116feaf8bb70baba3736db003804f13d50c29
ryanwatson <ryanwatson@salesforce.com>
2023-03-10 15:56:16 -0800

    Refactored location of meter aspect beans

--- src/main/java/org/watson/demos/configurations/MeterRegistryConfiguration.java
+++ src/main/java/org/watson/demos/configurations/MeterRegistryConfiguration.java
@@ -1,13 +1,15 @@
 package org.watson.demos.configurations;
 
+import io.micrometer.core.aop.CountedAspect;
+import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@ConditionalOnBean(MeterRegistry.class)
 @Configuration(proxyBeanMethods = false)
 public class MeterRegistryConfiguration {
 
@@ -15,5 +17,17 @@ public class MeterRegistryConfiguration {
     @Bean
     public MeterRegistryCustomizer<MeterRegistry> commonTags(final BuildProperties buildProperties) {
         return (registry) -> registry.config().commonTags("application.version", buildProperties.getVersion());
+    }
+
+    @ConditionalOnProperty(value = "spring.meter.aspect.enabled", matchIfMissing = true)
+    @Bean
+    public CountedAspect countedAspect(final MeterRegistry registry) {
+        return new CountedAspect(registry);
+    }
+
+    @ConditionalOnProperty(value = "spring.meter.aspect.enabled", matchIfMissing = true)
+    @Bean
+    public TimedAspect timedAspect(final MeterRegistry registry) {
+        return new TimedAspect(registry);
     }
 }
